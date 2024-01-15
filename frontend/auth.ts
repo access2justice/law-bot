bf9a24cfb0d61fb29f914ec761a28b72de0429c1
David Roegiers <david.roegiers@jurata.ch>
2024-01-15 09:36:06 +0100

    Revert "Update dependencies and fix GitHub provider in auth.ts"
    This reverts commit eababc98f7bc538217b2c95da0b1962f9be0d374.

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -14,7 +14,7 @@ export const {
   handlers: { GET, POST },
   auth
 } = NextAuth({
-  providers: [GitHub({}) as any],
+  providers: [GitHub],
   callbacks: {
     jwt({ token, profile }: { profile?: Profile; token: any }) {
       if (profile) {
@@ -28,6 +28,9 @@ export const {
         session.user.id = String(token.id)
       }
       return session
+    },
+    authorized({ auth }: { auth: Session | null }) {
+      return !!auth?.user // this ensures there is a logged in user for -every- request
     }
   },
   pages: {
