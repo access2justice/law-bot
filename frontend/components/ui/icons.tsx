afec1720fcf5c71ad2468756cc52329555fabca3
SOCRAMBLLE <mfragaprt@me.com>
2024-01-19 16:10:09 +0100

    Added Google svg icon

--- frontend/components/ui/icons.tsx
+++ frontend/components/ui/icons.tsx
@@ -137,6 +137,22 @@ function IconGitHub({ className, ...props }: React.ComponentProps<'svg'>) {
   )
 }
 
+function IconGoogle({ className, ...props }: React.ComponentProps<'svg'>) {
+  return (
+    <svg viewBox="0 0 48 48" className={cn('h-4 w-4', className)}>
+      <clipPath id="g">
+        <path d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z" />
+      </clipPath>
+      <g className="colors" clip-path="url(#g)">
+        <path fill="#FBBC05" d="M0 37V11l17 13z" />
+        <path fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z" />
+        <path fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z" />
+        <path fill="#4285F4" d="M48 48L17 24l-4-3 35-10z" />
+      </g>
+    </svg>
+  )
+}
+
 function IconSeparator({ className, ...props }: React.ComponentProps<'svg'>) {
   return (
     <svg
@@ -482,6 +498,7 @@ export {
   IconOpenAI,
   IconVercel,
   IconGitHub,
+  IconGoogle,
   IconSeparator,
   IconArrowDown,
   IconArrowRight,
