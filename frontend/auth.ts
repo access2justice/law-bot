25e1e2a9006c4d8f4f88cbf9105c03387e2240f2
SOCRAMBLLE <mfragaprt@me.com>
2024-01-19 16:01:41 +0100

    Added Google auth provider to NextAuth

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -1,39 +1,49 @@
-import NextAuth, { Session, type DefaultSession, Profile } from 'next-auth'
-import GitHub from 'next-auth/providers/github'
+import NextAuth, { Session, DefaultSession, Profile } from 'next-auth';
+import Google from 'next-auth/providers/google';
+import GitHub from 'next-auth/providers/github';
 
 declare module 'next-auth' {
   interface Session {
     user: {
       /** The user's id. */
-      id: string
-    } & DefaultSession['user']
+      id: string;
+    } & DefaultSession['user'];
   }
 }
 
 export const {
   handlers: { GET, POST },
-  auth
+  auth,
 } = NextAuth({
-  providers: [GitHub],
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID || '',
+      clientSecret: process.env.AUTH_GITHUB_SECRET || '',
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID || '',
+      clientSecret: process.env.AUTH_GOOGLE_SECRET || '',
+    }),
+  ],
   callbacks: {
     jwt({ token, profile }: { profile?: Profile; token: any }) {
       if (profile) {
-        token.id = profile.id
-        token.image = profile.avatar_url || profile.picture
+        token.id = profile.id;
+        token.image = profile.avatar_url || profile.picture;
       }
-      return token
+      return token;
     },
     session: ({ session, token }: { session: Session; token: any }) => {
       if (session?.user && token?.id) {
-        session.user.id = String(token.id)
+        session.user.id = String(token.id);
       }
-      return session
+      return session;
     },
     authorized({ auth }: { auth: Session | null }) {
-      return !!auth?.user // this ensures there is a logged in user for -every- request
-    }
+      return !!auth?.user; // this ensures there is a logged in user for -every- request
+    },
   },
   pages: {
-    signIn: '/sign-in' // overrides the next-auth default signin page https://authjs.dev/guides/basics/pages
-  }
-})
+    signIn: '/sign-in', // overrides the next-auth default signin page https://authjs.dev/guides/basics/pages
+  },
+});
