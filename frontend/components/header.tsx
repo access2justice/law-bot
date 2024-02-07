0a8ec02b992016b73a33630cfa1165c37f8d0696
David Roegiers <david.roegiers@jurata.ch>
2024-02-07 16:47:52 +0100

    Update Law Bot project branding and content

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+import lawBotTemporaryLogo from '../public/law-bot-temporary-logo.png'
 
 import { cn } from '@/lib/utils'
 import { auth } from '@/auth'
@@ -34,8 +36,13 @@ async function UserOrLogin() {
         </>
       ) : (
         <Link href="/" target="_blank" rel="nofollow">
-          <IconNextChat className="w-6 h-6 mr-2 dark:hidden" inverted />
-          <IconNextChat className="hidden w-6 h-6 mr-2 dark:block" />
+          <Image
+            className="w-6 h-6 transition-opacity duration-300 rounded-full select-none ring-1 ring-zinc-100/10 hover:opacity-80"
+            src={lawBotTemporaryLogo}
+            alt={'Law bot temporary logo'}
+            height={64}
+            width={64}
+          />
         </Link>
       )}
       <div className="flex items-center">
@@ -59,26 +66,6 @@ export function Header() {
         <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
           <UserOrLogin />
         </React.Suspense>
-      </div>
-      <div className="flex items-center justify-end space-x-2">
-        <a
-          target="_blank"
-          href="https://github.com/vercel/nextjs-ai-chatbot/"
-          rel="noopener noreferrer"
-          className={cn(buttonVariants({ variant: 'outline' }))}
-        >
-          <IconGitHub />
-          <span className="hidden ml-2 md:flex">GitHub</span>
-        </a>
-        <a
-          href="https://github.com/vercel/nextjs-ai-chatbot/"
-          target="_blank"
-          className={cn(buttonVariants())}
-        >
-          <IconVercel className="mr-2" />
-          <span className="hidden sm:block">Deploy to Vercel</span>
-          <span className="sm:hidden">Deploy</span>
-        </a>
       </div>
     </header>
   )
