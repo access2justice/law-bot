1230d10896620956813f51f4222eb15ff01700e5
ililcheva <iilcheva@centroida.co>
2024-02-09 12:35:01 +0100

    converted response to json

--- backend/src/api/chat.py
+++ backend/src/api/chat.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 from ..models.request import ChatRequest
 from ..services.ChatBotPipeline import ChatBotPipeline
 from ..services.globals import clients
@@ -14,5 +16,5 @@ async def chat_handler(chat_request: ChatRequest):
     model = os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME")
     chatbot = ChatBotPipeline(search_client, openai_client, model)
     response = await chatbot.run(chat_request)
-    return response
+    return JSONResponse(content=jsonable_encoder(response))
     
