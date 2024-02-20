a7cb9b9b1d35eded047df706e31e067922ee426b
SOCRAMBLLE <mfragaprt@me.com>
2024-02-20 18:41:53 +0100

    testing removing console log for the json

--- slack/api/slack/events.ts
+++ slack/api/slack/events.ts
@@ -9,47 +9,40 @@ export default async function MyEdgeFunction(
   context: RequestContext
 ) {
   console.log(1);
-  try {
-    const data = await req.json();
-    console.log(2);
+  const data = await req.json();
+  console.log(2);
 
-    console.log(data);
+  console.log(data);
 
-    if (data.type === "url_verification") {
-      return new Response(JSON.stringify({ challenge: data.challenge }), {
-        status: 200,
-        headers: { "content-type": "application/json" },
-      });
-    }
+  if (data.type === "url_verification") {
+    return new Response(JSON.stringify({ challenge: data.challenge }), {
+      status: 200,
+      headers: { "content-type": "application/json" },
+    });
+  }
 
-    if (
-      !data.event.thread_ts &&
-      !data.event.parent_user_id &&
-      data.event.type === "message" &&
-      data.type === "event_callback" &&
-      (data.event.channel === "C06GGJVRMCK" ||
-        data.event.channel === "C06HA3ZLB18")
-    ) {
-      context.waitUntil(
-        fetch(`https://${process.env.VERCEL_URL}/api/slack/process-events`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(data),
-        })
-          .then((json) => console.log({ json }))
-          .catch((error) =>
-            console.log("Error fetching process-events:", error)
-          )
-      );
-
-      return new Response(Date.now() + "");
-    }
+  if (
+    !data.event.thread_ts &&
+    !data.event.parent_user_id &&
+    data.event.type === "message" &&
+    data.type === "event_callback" &&
+    (data.event.channel === "C06GGJVRMCK" ||
+      data.event.channel === "C06HA3ZLB18")
+  ) {
+    context.waitUntil(
+      fetch(`https://${process.env.VERCEL_URL}/api/slack/process-events`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then(() => console.log("Process-events request sent"))
+        .catch((error) => console.log("Error fetching process-events:", error))
+    );
 
     return new Response(Date.now() + "");
-  } catch (error) {
-    console.error("Herror handling request:", error);
-    return new Response("Internal server error"), { status: 500 };
   }
+
+  return new Response(Date.now() + "");
 }
