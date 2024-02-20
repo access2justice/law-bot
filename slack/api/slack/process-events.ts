26747806f0ec78b4a37cd7884de48549210e9a05
SOCRAMBLLE <mfragaprt@me.com>
2024-02-20 20:28:44 +0100

    testing 0.5s of timeout

--- slack/api/slack/process-events.ts
+++ slack/api/slack/process-events.ts
@@ -8,7 +8,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   const data = body;
 
   try {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    await new Promise((resolve) => setTimeout(resolve, 500));
     const response = await fetch(process.env.AWS_API_CHAT_ENDPOINT || "", {
       method: "POST",
       headers: {
