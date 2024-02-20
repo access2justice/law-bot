9c94b025adcb50bc740b7c62f71779b6cd5ad464
David Roegiers <david.roegiers@jurata.ch>
2024-02-20 01:37:45 +0100

    Refactor Slack event handling code

--- slack/api/slack/events.ts
+++ slack/api/slack/events.ts
@@ -1,93 +1,97 @@
-import { WebClient } from '@slack/web-api'
-const web = new WebClient(process.env.SLACK_TOKEN)
+import { WebClient } from "@slack/web-api";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-export async function POST(req: Request) {
-  const data = await req.json()
+const web = new WebClient(process.env.SLACK_TOKEN);
 
-  if (data.type === 'url_verification') {
-    return Response.json({ challenge: data.challenge })
+export async function POST(req: VercelRequest, res: VercelResponse) {
+  console.log(1);
+  console.log(req.body);
+  const data = req.body;
+
+  if (data.type === "url_verification") {
+    return Response.json({ challenge: data.challenge });
   }
 
   if (
     !data.event.thread_ts &&
     !data.event.parent_user_id &&
-    data.event.type === 'message' &&
-    data.type === 'event_callback' &&
-    (data.event.channel === 'C06GGJVRMCK' || 'C06HA3ZLB18')
+    data.event.type === "message" &&
+    data.type === "event_callback" &&
+    (data.event.channel === "C06GGJVRMCK" || "C06HA3ZLB18")
   ) {
-    handleRequests(data)
+    handleRequests(data);
 
     return new Response(data.challenge, {
-      status: 200
-    })
+      status: 200,
+    });
   }
 
-  return new Response('Ok', {
-    status: 200
-  })
+  return new Response("Ok", {
+    status: 200,
+  });
 }
 
 async function handleRequests(data: any) {
-  console.log(1)
-  const response = await fetch(process.env.AWS_API_CHAT_ENDPOINT || '', {
-    method: 'POST',
+  console.log(1);
+  const response = await fetch(process.env.AWS_API_CHAT_ENDPOINT || "", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       message: [
         {
-          role: 'user',
+          role: "user",
           content:
-            data.event.text || 'Explain to the user that something went wrong.'
-        }
-      ]
-    })
-  })
-  console.log(2)
+            data.event.text || "Explain to the user that something went wrong.",
+        },
+      ],
+    }),
+  });
+  console.log(2);
 
-  const json = await response.json()
-  console.log(3)
+  const json = await response.json();
+  console.log(3);
 
   const payload_value = JSON.stringify({
     user_input: data.event.text,
-    ai_response: json.data.content
-  })
+    ai_response: json.data.content,
+  });
 
-  console.log(4)
+  console.log(4);
 
   const messageBlocks = [
     {
-      type: 'section',
+      type: "section",
       text: {
-        type: 'mrkdwn',
-        text: json.data.content
-      }
+        type: "mrkdwn",
+        text: json.data.content,
+      },
     },
     {
-      type: 'actions',
+      type: "actions",
       elements: [
         {
-          type: 'button',
+          type: "button",
           text: {
-            type: 'plain_text',
-            text: 'Share a feedback'
+            type: "plain_text",
+            text: "Share a feedback",
           },
-          action_id: 'feedback',
-          value: payload_value
-        }
-      ]
-    }
-  ]
+          action_id: "feedback",
+          value: payload_value,
+        },
+      ],
+    },
+  ];
 
-  console.log(5)
+  console.log(5);
 
   await web.chat.postMessage({
     blocks: messageBlocks,
     thread_ts: data.event.ts,
     channel: data.event.channel,
-    text: json.data.content
-  })
+    text: json.data.content,
+  });
 
-  console.log(6)
+  console.log(6);
 }
