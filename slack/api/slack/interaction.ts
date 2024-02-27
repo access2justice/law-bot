eb330b7846bf89cee0525476447deb1a6eb8fa5a
SOCRAMBLLE <mfragaprt@me.com>
2024-02-27 14:43:27 +0100

    Refactor interaction.ts to improve logging

--- slack/api/slack/interaction.ts
+++ slack/api/slack/interaction.ts
@@ -56,13 +56,12 @@ export default async function POST(req: VercelRequest, res: VercelResponse) {
       }
     } else if (payload.type === "view_submission") {
       console.log("payload.view.blocks:", JSON.stringify(payload.view.blocks));
-
       const submittedValues = payload.view.state.values;
-      console.log("submittedValues:", submittedValues);
       console.log(
         "submittedValues[0]['static_select-action']['selected_option']:",
         submittedValues[0]["static_select-action"]["selected_option"]
       );
+      console.log("submittedValues:", submittedValues);
 
       try {
         const correct =
