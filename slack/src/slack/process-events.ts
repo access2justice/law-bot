19a9d934f2a860340475773c7e12af0fd2897cb7
SOCRAMBLLE <mfragaprt@me.com>
2024-03-13 22:40:01 +0100

    Refactor processEvents function to include error handling and logging

--- slack/src/slack/process-events.ts
+++ slack/src/slack/process-events.ts
@@ -13,25 +13,26 @@ interface ApiResponse {
   };
 }
 
-export default async function handler(data: any) {
+export default async function processEvents(data: any) {
   console.log('2. Initiate process-events, data:' + JSON.stringify(data));
-  // try {
-  //   console.log('2.1 Start message' + new Date());
-  //   const postMessageResponse = await web.chat.postMessage({
-  //     thread_ts: data.event.ts,
-  //     channel: data.event.channel,
-  //     text: 'Thanks for your message, one moment please ...',
-  //   });
-  //   console.log(
-  //     '2.2 First response slack message:',
-  //     JSON.stringify(postMessageResponse),
-  //   );
-  // } catch (e) {
-  //   console.error('2.2 Error sending message:', JSON.stringify(e, null, 2));
-  // }
   try {
-    // console.log('2.3. Start message:', new Date());
-    // await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log('2.1 Start message' + new Date());
+    const postMessageResponse = await web.chat.postMessage({
+      thread_ts: data.event.ts,
+      channel: data.event.channel,
+      text: 'Thanks for your message, one moment please ...',
+    });
+    console.log(
+      '2.2 First response slack message:',
+      JSON.stringify(postMessageResponse),
+    );
+  } catch (e) {
+    console.log('2.2 Error sending message:', e);
+    console.error('2.2 Error sending message:', JSON.stringify(e, null, 2));
+  }
+  try {
+    console.log('2.3. Start message:', new Date());
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     console.log('2.4. Fetching Backend:', new Date());
     const response = await fetch(process.env.AWS_API_CHAT_ENDPOINT || '', {
       method: 'POST',
