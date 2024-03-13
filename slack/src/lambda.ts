f367bebb613d869931c27f2b7cd28348e8d878dc
David Roegiers <david.roegiers@jurata.ch>
2024-03-13 22:40:02 +0100

    try this

--- slack/src/lambda.ts
+++ slack/src/lambda.ts
@@ -1,4 +1,4 @@
-import serverlessExpress from '@codegenie/serverless-express';
+import { configure as serverlessExpress } from '@codegenie/serverless-express';
 import app from './app';
 
 exports.handler = serverlessExpress({ app });
