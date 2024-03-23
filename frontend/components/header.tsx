b9756d5cfcd12b9aa0f68f105be8ff3439713405
SOCRAMBLLE <mfragaprt@me.com>
2024-03-23 22:47:17 +0100

    Fix

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -78,26 +78,24 @@ export function Header() {
           <UserOrLogin />
         </React.Suspense>
       </div>
-      <Link href={path === '/chat/expert' ? '/' : '/chat/expert'}>
-        <label
-          className="inline-flex items-center cursor-pointer"
-          onClick={handleToggle}
-        >
-          <input
-            type="checkbox"
-            value=""
-            checked={expertMode}
-            onChange={() => {}}
-            className="sr-only peer"
-          />
-          <div
-            className={`relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer ${expertMode ? 'peer-checked:bg-blue-600' : ''}`}
-          ></div>
-          <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-            {expertMode ? 'Expert Mode' : 'Normal Mode'}
-          </span>
-        </label>
-      </Link>
+      <label
+        className="inline-flex items-center cursor-pointer"
+        onClick={handleToggle}
+      >
+        <input
+          type="checkbox"
+          value=""
+          checked={expertMode}
+          onChange={() => {}}
+          className="sr-only peer"
+        />
+        <div
+          className={`relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer ${expertMode ? 'peer-checked:bg-blue-600' : ''}`}
+        ></div>
+        <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+          {expertMode ? 'Expert Mode' : 'Normal Mode'}
+        </span>
+      </label>
     </header>
   )
 }
