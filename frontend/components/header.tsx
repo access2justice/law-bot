a0e2919b0feef2b7efe1195bc2488a47353d4082
SOCRAMBLLE <mfragaprt@me.com>
2024-03-31 17:39:15 +0200

    Refactor Header component to use async/await for authentication

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -62,7 +62,8 @@ async function UserOrLogin() {
   )
 }
 
-export function Header() {
+export async function Header() {
+  const session = await auth()
   return (
     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
       <div className="flex items-center">
@@ -70,9 +71,11 @@ export function Header() {
           <UserOrLogin />
         </React.Suspense>
       </div>
-      <div className="flex items-center justify-end space-x-2">
-        <ExpertToggle />
-      </div>
+      {session?.user && (
+        <div className="flex items-center justify-end space-x-2">
+          <ExpertToggle />
+        </div>
+      )}
     </header>
   )
 }
